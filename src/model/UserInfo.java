1b2c5395dda6b79182023163869c9af8e518b8a7
Livia123456 <122884168+Livia123456@users.noreply.github.com>
2023-04-18 12:15:33 +0200

    Revert "Merge branch 'update-email-class'"
    This reverts commit 0d43ad0061fa49c905ba2b8417a521544ee56c3b, reversing
    changes made to 0c7f1f29e9102f0c889dca5de86c3caf237ea900.

--- src/model/UserInfo.java
+++ src/model/UserInfo.java
@@ -9,8 +9,8 @@ public class UserInfo implements Serializable {
     private String name;
     private boolean correctInfo = false;
 
-    public UserInfo(String email, String password) {
-        this.email = email;
+    public UserInfo(String name, String password) {
+        this.email = name;
         this.password = password;
     }
 
