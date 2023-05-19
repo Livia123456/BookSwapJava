3c4a01ce5b68f18155ecadbbd0d2c258f2a56f8c
SebastianZulj <127675391+SebastianZulj@users.noreply.github.com>
2023-05-19 11:19:16 +0200

    UserInfo har nu builderklass

--- src/model/UserInfo.java
+++ src/model/UserInfo.java
@@ -24,6 +24,8 @@ public class UserInfo implements Serializable {
 
 //TODO skapa builder!!
 
+    public UserInfo() {}
+
     public UserInfo(Email email, String password) {
         this.email = email;
         this.password = password;
@@ -102,5 +104,57 @@ public class UserInfo implements Serializable {
     }
     public void setProfileImage(ImageIcon profileImage) {
         this.profileImage = profileImage;
+    }
+
+    private class UserBuilder {
+        private UserInfo userInfo;
+
+        public UserBuilder() {
+            userInfo = new UserInfo();
+        }
+
+        public UserBuilder email(Email email){
+            userInfo.email = email;
+            return this;
+        }
+
+        public UserBuilder name(String name) {
+            userInfo.name = name;
+            return this;
+        }
+
+        public UserBuilder userId(int userId) {
+            userInfo.userId = userId;
+            return this;
+        }
+
+        public UserBuilder password(String password) {
+            userInfo.password = password;
+            return this;
+        }
+
+        public UserBuilder correctInfo(boolean correctInfo) {
+            userInfo.correctInfo = correctInfo;
+            return this;
+        }
+
+        public UserBuilder uploadedBooks(ArrayList<Book> uploadedBooks) {
+            userInfo.currentUsersUploadedBooks = uploadedBooks;
+            return this;
+        }
+
+        public UserBuilder chatsWith(ArrayList<ChatsWith> chatsWiths) {
+            userInfo.chatsWith = chatsWiths;
+            return this;
+        }
+
+        public UserBuilder profileImage(ImageIcon imageIcon) {
+            userInfo.profileImage = imageIcon;
+            return this;
+        }
+
+        public UserInfo build() {
+            return userInfo;
+        }
     }
 }
